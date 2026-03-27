lowRISC/opentitan

[tests] run E2E orchestrator tests on all SKUs
This runs the E2E orchestrator provisioning test on all SKUs. Currently,
E2E provisioning flows are also runnable via an `opentitan_test` rule.
These will be removed in a future commit to simplify SKU management.

Signed-off-by: Tim Trippel <ttrippel@google.com>

sw/host/provisioning/orchestrator/tests/transition.bzl
@@ -2,14 +2,29 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-def _orchestrator_settings_impl(settings, attr):
+def _orchestrator_hyper310_settings_impl(settings, attr):
     return {
         "//hw/bitstream/universal:otp": "//hw/ip/otp_ctrl/data/earlgrey_skus/emulation:otp_img_test_unlocked0_manuf_empty",
         "//hw/bitstream/universal:env": "//hw/top_earlgrey:fpga_hyper310_rom_with_fake_keys",
     }
 
-_orchestrator_settings = transition(
-    implementation = _orchestrator_settings_impl,
+_orchestrator_hyper310_settings = transition(
+    implementation = _orchestrator_hyper310_settings_impl,
+    inputs = [],
+    outputs = [
+        "//hw/bitstream/universal:otp",
+        "//hw/bitstream/universal:env",
+    ],
+)
+
+def _orchestrator_cw340_settings_impl(settings, attr):
+    return {
+        "//hw/bitstream/universal:otp": "//hw/ip/otp_ctrl/data/earlgrey_skus/emulation:otp_img_test_unlocked0_manuf_empty",
+        "//hw/bitstream/universal:env": "//hw/top_earlgrey:fpga_cw340_rom_with_fake_keys",
+    }
+
+_orchestrator_cw340_settings = transition(
+    implementation = _orchestrator_cw340_settings_impl,
     inputs = [],
     outputs = [
         "//hw/bitstream/universal:otp",
@@ -26,9 +41,20 @@ def _orchestrator_test_settings_transition_impl(ctx):
         ),
     ]
 
-orchestrator_test_settings_transition = rule(
+orchestrator_hyper310_test_settings_transition = rule(
     implementation = _orchestrator_test_settings_transition_impl,
-    cfg = _orchestrator_settings,
+    cfg = _orchestrator_hyper310_settings,
+    attrs = {
+        "target": attr.label(),
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    },
+)
+
+orchestrator_cw340_test_settings_transition = rule(
+    implementation = _orchestrator_test_settings_transition_impl,
+    cfg = _orchestrator_cw340_settings,
     attrs = {
         "target": attr.label(),
         "_allowlist_function_transition": attr.label(

vlpannel/pavona

(cherry picked from commit 5e0c7952b1b11bd62fca18e1675ecb9ee59a0551)

sw/host/provisioning/orchestrator/tests/transition.bzl
@@ -2,14 +2,29 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
-def _orchestrator_settings_impl(settings, attr):
+def _orchestrator_cw310_settings_impl(settings, attr):
     return {
         "//hw/bitstream/universal:otp": "//hw/top_earlgrey/data/otp/emulation:otp_img_test_unlocked0_manuf_empty",
         "//hw/bitstream/universal:env": "//hw/top_earlgrey:fpga_cw310_rom_with_fake_keys",
     }
 
-_orchestrator_settings = transition(
-    implementation = _orchestrator_settings_impl,
+_orchestrator_cw310_settings = transition(
+    implementation = _orchestrator_cw310_settings_impl,
+    inputs = [],
+    outputs = [
+        "//hw/bitstream/universal:otp",
+        "//hw/bitstream/universal:env",
+    ],
+)
+
+def _orchestrator_cw340_settings_impl(settings, attr):
+    return {
+        "//hw/bitstream/universal:otp": "//hw/top_earlgrey/data/otp/emulation:otp_img_test_unlocked0_manuf_empty",
+        "//hw/bitstream/universal:env": "//hw/top_earlgrey:fpga_cw340_rom_with_fake_keys",
+    }
+
+_orchestrator_cw340_settings = transition(
+    implementation = _orchestrator_cw340_settings_impl,
     inputs = [],
     outputs = [
         "//hw/bitstream/universal:otp",
@@ -26,9 +41,20 @@ def _orchestrator_test_settings_transition_impl(ctx):
         ),
     ]
 
-orchestrator_test_settings_transition = rule(
+orchestrator_cw310_test_settings_transition = rule(
     implementation = _orchestrator_test_settings_transition_impl,
-    cfg = _orchestrator_settings,
+    cfg = _orchestrator_cw310_settings,
+    attrs = {
+        "target": attr.label(),
+        "_allowlist_function_transition": attr.label(
+            default = "@bazel_tools//tools/allowlists/function_transition_allowlist",
+        ),
+    },
+)
+
+orchestrator_cw340_test_settings_transition = rule(
+    implementation = _orchestrator_test_settings_transition_impl,
+    cfg = _orchestrator_cw340_settings,
     attrs = {
         "target": attr.label(),
         "_allowlist_function_transition": attr.label(